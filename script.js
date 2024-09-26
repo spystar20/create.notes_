84421278a1c8fa575d53da3d9e1fa107275754a1
spystar20 <hert5197@gmail.com>
2024-09-26 19:32:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
+
 const noteBox = document.querySelector(".parent");
 const btn = document.querySelector(".btn");
-let page = document.querySelectorAll(".input");
+let notes = document.querySelectorAll(".box");
 
-function showNotes(){
-noteBox.innerHTML= localStorage.getItem('notes')
+function showNotes() {
+  noteBox.innerHTML = localStorage.getItem("notes");
 }
-showNotes()
+showNotes();
 
-function update(){
-    localStorage.setItem("notes" , noteBox.innerHTML)
+function update() {
+  localStorage.setItem("notes", noteBox.innerHTML);
 }
 
 btn.addEventListener("click", () => {
-  let div= document.createElement("div");
+  let div = document.createElement("div");
   let inputBox = document.createElement("p");
   let divBox = document.createElement("div");
   let img = document.createElement("img");
-  img.className= "img"
+
   div.className = "bound";
   inputBox.className = "input";
   inputBox.setAttribute("contenteditable", "true");
+
   divBox.className = "box";
+  divBox.setAttribute("contenteditable", "true");
+  img.className = "img";
   img.src = "bin_484611-removebg-preview.png";
 
-  noteBox.appendChild(div).appendChild(inputBox).appendChild(divBox)
+  noteBox.appendChild(div).appendChild(inputBox).appendChild(divBox);
   div.appendChild(img);
-
 });
 
 noteBox.addEventListener("click", function (e) {
-  if (e.target.tagName ==="IMG") {
-    
+  if (e.target.tagName === "IMG") {
     e.target.parentElement.remove();
-    update()
-  }
-  else if(e.target.tagName==="P"){
-    notes = document.querySelectorAll(".input")
+    update();
+  } else if (e.target.tagName === "DIV") {
+    notes = document.querySelectorAll(".box");
     notes.forEach(notes => {
-        notes.onkeyup = function () {
-            update()
-        }
-        
+      notes.onkeyup = function () {
+        update();
+      };
     });
   }
 });
