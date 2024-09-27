7aaceea1f17cb0f6a669f9584bbe17df90aed783
spystar20 <hert5197@gmail.com>
2024-09-27 23:47:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,41 @@
-const noteBox = document.querySelector(".parent");
-const btn = document.querySelector(".btn");
 
-function showNotes() {
-  noteBox.innerHTML = localStorage.getItem("notes") || "";
-  reinitializeEvents();
+const parentDiv = document.querySelector(".parent");
+const btn = document.querySelector(".button");
+
+function check() {
+  parentDiv.innerHTML = localStorage.getItem("notes");
 }
-showNotes();
-function updateNotes() {
-  localStorage.setItem("notes", noteBox.innerHTML);
+check()
+function save() {
+  localStorage.setItem("notes", parentDiv.innerHTML);
 }
 
 btn.addEventListener("click", () => {
-  const div = document.createElement("div");
-  div.className = "bound";
-
-  const inputBox = document.createElement("p");
-  inputBox.className = "input";
-  inputBox.setAttribute("contenteditable", "true");
-
+  const parentDiv = document.querySelector(".parent");
   const divBox = document.createElement("div");
-  divBox.className = "box";
-  divBox.setAttribute("contenteditable", "true");
-
+  divBox.className = "bound";
+  const input = document.createElement("para");
+  input.className = "input";
+  input.setAttribute("contenteditable", "true");
+  const div = document.createElement("div");
+  div.className = "box";
+  div.setAttribute("contenteditable", "true");
   const img = document.createElement("img");
-  img.className = "img";
   img.src = "bin_484611-removebg-preview.png";
+  img.className = "img";
+  parentDiv.appendChild(divBox);
+  divBox.appendChild(input).appendChild(div);
+  divBox.appendChild(img);
 
-  div.append(inputBox, divBox, img);
-  noteBox.appendChild(div);
-  updateNotes();
-  reinitializeEvents();
-});
-
-noteBox.addEventListener("click", (e) => {
-  if (e.target.tagName === "IMG") {
-    e.target.parentElement.remove();
-    updateNotes();
-  }
-});
-
-function reinitializeEvents() {
-  noteBox.querySelectorAll(".box").forEach(note => {
-    note.onkeyup = updateNotes;
+  divBox.querySelector(".img").addEventListener("click", () => {
+    divBox.remove();
+    save();
   });
-}
+
+  divBox.addEventListener("keyup", () => {
+    let notes = document.querySelectorAll(".box");
+    notes.forEach((notes) => {
+      save();
+    });
+  });
+});
